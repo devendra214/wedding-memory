ef4b351d0408658f442beb17c8cf53ff3c954917
Devendra Yogi <yogidrvendra786@gmail.com>
2026-06-24 15:09:36 +0530

    render settings

--- wedding_memory/settings.py
+++ wedding_memory/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d3@ua+e1q5^il&^%+w!kprx&n##ny0m%*nos6737i&$vgtzu@j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
